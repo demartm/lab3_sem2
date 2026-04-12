e9d91b6747838ed5ce41fd04a94d858af86b2177
demartm <artemis20242511@gmail.com>
2026-04-12 03:38:25 -0400

    added str to bv convertation

--- main.c
+++ main.c
@@ -1,4 +1,43 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+unsigned char *convertLongBvToStr(unsigned char *vec, size_t sz){
+if(vec){
+
+  int len = 8 * sz + 1;
+  unsigned char *str = (unsigned char*)calloc(len, sizeof(unsigned char));
+
+  if(str){
+
+  int ix = 0;
+
+    for(int i = 0; i < len; i++){
+      unsigned char mask = 1;
+
+      for(int j = 0; (j < 8) && (ix < len); j++){
+
+        if(vec[i] & mask){
+
+          str[ix] = '1';
+        } else {
+
+          str[ix] = '0';
+        }
+
+        mask = mask << 1;
+        ix++;
+
+    }
+
+  }
+
+  return str;
+  }
+
+}
+return NULL;
+}
 
 int main()
 {
