0d6f9569b311c089eb211e4d09d91eb502008a57
demartm <artemis20242511@gmail.com>
2026-04-12 07:27:40 -0400

    added set bit func

--- main.c
+++ main.c
@@ -1,4 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool set_bit(unsigned char *vec, bool bit, size_t k){
+if(vec){
+
+  int byte = k / 8;
+  int bit = k % 8;
+  unsigned char mask = 1;
+  // if(!bit){
+  //   mask = 0;
+  // }
+
+  mask = mask << bit;
+  if(bit){
+  vec[byte] |= mask;
+
+  } else {
+  vec[byte] &= ~mask;
+  }
+
+  return true;
+
+}
+return false;
+}
 
 int main()
 {
