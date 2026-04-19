a30f9fb4a64cc8080686b70a4ff269906f9ac10c
demartm <artemis20242511@gmail.com>
2026-04-19 12:41:11 -0400

    updated func

--- main.c
+++ main.c
@@ -2,41 +2,40 @@
 #include <string.h>
 #include <stdlib.h>
 
-unsigned char *convertLongBvToStr(unsigned char *vec, size_t sz){
-if(vec){
+unsigned char *convertLongBvToStr(char *vec, size_t bytes){
 
-  int len = 8 * sz + 1;
-  unsigned char *str = (unsigned char*)calloc(len, sizeof(unsigned char));
+if(vec && bytes){
+  size_t len = bytes * 8;
+
+  size_t ix = 0;
+  unsigned char *str = (unsigned char*)calloc(len + 1, sizeof(unsigned char));
 
   if(str){
 
-  int ix = 0;
-
-    for(int i = 0; i < len; i++){
+    for(size_t i = 0; i < len; i++){
       unsigned char mask = 1;
 
-      for(int j = 0; (j < 8) && (ix < len); j++){
+      for(size_t j = 0; (j < 8) && (ix < len); j ++){
 
         if(vec[i] & mask){
-
           str[ix] = '1';
         } else {
-
           str[ix] = '0';
         }
 
-        mask = mask << 1;
         ix++;
+        mask = mask << 1;
+      }
 
     }
-
-  }
-
+  str[len + 1] = '\0';
   return str;
   }
 
+
 }
 return NULL;
+
 }
 
 int main()
