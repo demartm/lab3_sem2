3fe08d2cc9cc5802b89705f44ec540f927cd1c63
demartm <artemis20242511@gmail.com>
2026-04-19 06:16:10 -0400

    added log sum and multiply

--- main.c
+++ main.c
@@ -1,4 +1,49 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+unsigned char *logMul(unsigned char *vecA, unsigned char *vecB, size_t bitsA, size_t bitsB){
+
+if(vecA && vecB && bitsA && bitsB && (bitsA == bitsB)){
+
+
+  size_t bytes = (bitsA + 7) / 8;
+
+  unsigned char *result = (unsigned char*)calloc(bytes, sizeof(unsigned char));
+
+    if(result){
+
+      for(size_t i = 0; i < bytes; i++){
+
+        result[i] = vecA[i] & vecB[i];
+      }
+    }
+return result;
+}
+return NULL;
+}
+
+unsigned char *logSum(unsigned char *vecA, unsigned char *vecB, size_t bitsA, size_t bitsB){
+
+if(vecA && vecB && bitsA && bitsB && (bitsA == bitsB)){
+
+
+  size_t bytes = (bitsA + 7) / 8;
+
+  unsigned char *result = (unsigned char*)calloc(bytes, sizeof(unsigned char));
+
+    if(result){
+
+      for(size_t i = 0; i < bytes; i++){
+
+        result[i] = vecA[i] | vecB[i];
+      }
+    }
+return result;
+}
+return NULL;
+}
 
 int main()
 {
