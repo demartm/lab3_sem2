241170f8d063f5488cc1c20b6ec10116aa651231
demartm <artemis20242511@gmail.com>
2026-04-19 11:57:05 -0400

    updated printbv

--- main.c
+++ main.c
@@ -2,6 +2,37 @@
 #include <string.h>
 #include <stdlib.h>
 
+void printBV(unsigned char *vec, size_t bits){
+if(vec && bits){
+
+  size_t bytes = ((bits - 1) / 8) + 1;
+  size_t ix = 0;
+
+  for(size_t i = 0; i < bytes; i++){
+
+    unsigned char mask = 1;
+
+    for(size_t j = 0; j < 8 && (ix < bits); j++){
+
+      if(vec[i] & mask){
+
+        printf("1");
+      } else {
+
+        printf("0");
+      }
+
+      mask = mask << 1;
+      ix++;
+    }
+
+  }
+
+
+}
+
+}
+
 unsigned char *convertStrToLongBv(char *str, int *cells) {
 
 if(str && cells){
