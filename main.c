8b5474317cf770bfbdf32fe38755e961b67ca63a
demartm <artemis20242511@gmail.com>
2026-04-19 13:55:57 -0400

    correct order

--- main.c
+++ main.c
@@ -65,6 +65,41 @@ return vec;
 }
 }
 }
+unsigned char *convertLongBvToStr(char *vec, size_t bytes){
+
+if(vec && bytes){
+  size_t len = bytes * 8;
+
+  size_t ix = 0;
+  unsigned char *str = (unsigned char*)calloc(len + 1, sizeof(unsigned char));
+
+  if(str){
+
+    for(size_t i = 0; i < len; i++){
+      unsigned char mask = 1;
+
+      for(size_t j = 0; (j < 8) && (ix < len); j ++){
+
+        if(vec[i] & mask){
+          str[ix] = '1';
+        } else {
+          str[ix] = '0';
+        }
+
+        ix++;
+        mask = mask << 1;
+      }
+
+    }
+  str[len + 1] = '\0';
+  return str;
+  }
+
+
+}
+return NULL;
+
+}
 
 void inversion(unsigned char *vec, size_t len){//bool invert_vec(unsigned char* vec, size_t len){
 if(vec){
@@ -121,41 +156,7 @@ if(vec && bits && bit && (bits >= bit)){
 
 }
 
-unsigned char *convertLongBvToStr(char *vec, size_t bytes){
 
-if(vec && bytes){
-  size_t len = bytes * 8;
-
-  size_t ix = 0;
-  unsigned char *str = (unsigned char*)calloc(len + 1, sizeof(unsigned char));
-
-  if(str){
-
-    for(size_t i = 0; i < len; i++){
-      unsigned char mask = 1;
-
-      for(size_t j = 0; (j < 8) && (ix < len); j ++){
-
-        if(vec[i] & mask){
-          str[ix] = '1';
-        } else {
-          str[ix] = '0';
-        }
-
-        ix++;
-        mask = mask << 1;
-      }
-
-    }
-  str[len + 1] = '\0';
-  return str;
-  }
-
-
-}
-return NULL;
-
-}
 
 
 unsigned char *logMul(unsigned char *vecA, unsigned char *vecB, size_t bitsA, size_t bitsB){
